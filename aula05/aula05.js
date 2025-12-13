9b56afec21148f0999665b8ef90347c4b8ba6304
Arthur Gomes <arthuur.uvv@gmail.com>
2025-12-12 21:49:21 -0300

    Aula 05 do curso

---
+++ aula05/aula05.js
@@ -0,0 +1,34 @@
+//comentário
+
+/*
+mais de uma linha
+de comentário
+*/
+
+//AULA SOBRE VARIÁVEIS E TIPOS PRIMITIVOS
+
+var a //var cria uma variável e define seu nome
+
+//a é um identificador
+
+/* IDENTIFICADORES
+Podem começar com letra, $ ou _
+Não podem começar com números
+É possível usar letras ou números
+É possível usar acentos e símbolos
+Não podem conter espaços
+Não podem ser palavras reservadas (ex: fuction, var)
+*/
+
+/* DATA TYPES
+number
+    Infinity
+    NaN
+string
+boolean
+null
+undefined
+object
+    Array
+function
+*/
