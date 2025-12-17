05a0f219f795c7f548e0d39508fe82a475535855
Arthur Gomes <arthuur.uvv@gmail.com>
2025-12-16 21:03:17 -0300

    Exercitando condições e DOM

---
+++ modulo-d/aula11/ex11-01.js
@@ -0,0 +1,8 @@
+var vel = 60
+console.log(`A velocidae do seu carro é ${vel}Km/h`)
+if (vel > 60){
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+} else {
+    console.log('Tudo certo e boa viagem!')
+}
+console.log('Dirija sempre com cuidado e use o cinto de segurança.')
---
+++ modulo-d/modulo-d.md
@@ -0,0 +1,11 @@
+# Condições
+
+## Comando if
+`if (condicao){
+    true
+} else {
+    false
+}`
+
+## Tipos de Condição
+Condição simples = não tem else, else if.
