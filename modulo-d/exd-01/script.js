02429054815d9432578a68a71835ed345645ec23
Arthur Gomes <arthuur.uvv@gmail.com>
2025-12-18 23:14:32 -0300

    atv1 concluida

--- modulo-d/exd-01/script.js
+++ modulo-d/exd-01/script.js
@@ -1,17 +1,23 @@
 var dataAtual = new Date()
-
 var hora = dataAtual.getHours()
+var textoHora = window.document.getElementById('textoHora')
+var imgHora = window.document.getElementById('imgHora')
 
-if (hora <= 6 ){
-    var textoHora = window.document.getElementById(textoHora)
-    var imgHora = window.document.getElementById(imgHora)
+textoHora.innerHTML = `Agora são ${hora} horas`
 
-    textoHora.innerHTML = `Agora são ${hora} horas da madrugada`
+if (hora <= 5){
+    imgHora.src = './imagens/img-madrugada.png'
+    document.body.style.background = '#000000ff'
 }
-
-else if (hora >= 18){
-    var txtHora = window.document.getElementById(textoHora).value
-    var imgHora = window.document.getElementById(imgHora)
-
-    txtHora.innerHTML = `Agora são ${hora} horas da noite`
+else if (hora <= 11){
+    imgHora.src = './imagens/img-manha.jpg'
+    document.body.style.background = '#db8f00ff'
+}
+else if (hora <= 17){
+    imgHora.src = './imagens/img-tarde.jpg'
+    document.body.style.background = '#ffe600ff'
+}
+else{
+    imgHora.src = './imagens/img-noite.jpg'
+    document.body.style.background = '#020075ff'
 }
