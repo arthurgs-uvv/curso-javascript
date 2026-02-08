8951da5b2a1a887c29294151c41373e3a28af558
Arthur Gomes <arthuur.uvv@gmail.com>
2026-02-08 20:36:05 -0300

    Resolvendo mais exercícios

--- exercícios-nvl1/script-10.js
+++ exercícios-nvl1/script-10.js
@@ -1,3 +1,62 @@
-function analisador(arr){
-    
+/*
+{
+  total: quantidade de números,
+  soma: soma dos valores,
+  maior: maior valor,
+  menor: menor valor,
+  media: média
 }
+*/
+
+function total(arr){
+    return arr.length
+}
+
+function soma(arr){
+    let soma = 0
+
+    for(let a in arr){
+        soma += arr[a]
+    }
+
+    return soma
+}
+
+function maior(arr){
+    arr.sort(function(a, b){return a-b})
+
+    return arr[arr.length - 1]
+}
+
+function menor(arr){
+    arr.sort(function(a,b){return a-b})
+
+    return arr[0]
+}
+
+function media(arr){
+    let media = 0
+
+    for(let a in arr){
+        media += arr[a]
+    }
+    
+    return media/(Number(arr.length))
+}
+
+function montandoObjeto(arr){
+    let objeto = {
+        total: total(arr),
+        soma: soma(arr),
+        maior: maior(arr),
+        menor: menor(arr),
+        media: media(arr)
+    }
+
+    return objeto 
+}
+
+let numeros = [3, 7, 1, 9, 4, 6]
+
+console.log(numeros + "\n")
+console.log(montandoObjeto(numeros))
